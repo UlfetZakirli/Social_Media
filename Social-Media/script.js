1edecae8705d8090edc5f70698dd04decd42a506
UlfetZakirli <88549805+UlfetZakirli@users.noreply.github.com>
2022-01-26 16:33:21 +0400

    Add files via upload

---
+++ Social-Media/script.js
@@ -0,0 +1,6 @@
+var share_btn=document.querySelector('.share_btn');
+var toggle_button=document.querySelector('.toggle_button');
+
+share_btn.addEventListener('click',function(){
+    toggle_button.classList.toggle('active')
+})
